359e779927e8e2d41d0f7c0b4a1c52f288fc2f61
mranch <anchiforov@gmail.com>
2020-03-17 15:56:50 +0200

    Added header with message length and created command to order drink

--- client.py
+++ client.py
@@ -4,11 +4,43 @@ import pickle
 
 IP = '127.0.0.1'
 PORT = 1234
+HEADER_SIZE = 10
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((IP, PORT))
-d = {
-    "first": 1
-}
-d = pickle.dumps(d)
-s.send(d)
+
+class MakeDrinkCommand:
+    def __init__(self, data):
+        self._data = data
+        self.dumped_data = pickle.dumps(self._data)
+        self.data_size = len(self.dumped_data)
+
+    def send(self, server_socket):
+        server_socket.send(bytes(f"{self.data_size:<{HEADER_SIZE}}", "utf-8") + self.dumped_data)
+
+
+# example of order
+# {
+#   drink: coffee
+
+# optional:
+#   add: milk
+# }
+def order_drink(order, sock):
+    make_drink_command = MakeDrinkCommand(order)
+    make_drink_command.send(sock)
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+    first_order = {
+        "drink": "coffee",
+        "add": "milk"
+    }
+    second_order = {
+        "drink": "soda"
+    }
+    order_drink(first_order, s)
+    while True:
+        resp = s.recv(100)
+
+        print(pickle.loads(resp))
--- server.py
+++ server.py
@@ -1,14 +1,41 @@
 import socket
 import pickle
+from functools import reduce
 
 IP = '127.0.0.1'
 PORT = 1234
+drinks = ['coffee', 'water', 'soda']
+adds = ['milk', 'sugar']
+HEADER_SIZE = 10
+goods = {
+    "drinks": {
+        "coffee": 10,
+        "water": 20,
+        "soda": 30
+    },
+    "adds": {
+        "milk": 7,
+        "sugar": 8
+    }
+}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((IP, PORT))
-s.listen(5)
 
-while True:
-    client_socket, address = s.accept()
-    message = client_socket.recv(100)
-    print(pickle.loads(message))
+def receive_order(client_sock):
+    message = client_sock.recv(100)
+    message_len = int(message[:HEADER_SIZE])
+    print(f"Message_len {message_len}")
+    message = pickle.loads(message[HEADER_SIZE:])
+    print(message)
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((IP, PORT))
+    s.listen(5)
+
+    while True:
+        client_socket, address = s.accept()
+        receive_order(client_socket)
+        # resp = bytes(f"'you sent': {message}", "utf-8")
+        # resp = pickle.dumps(resp)
+        # client_socket.send(resp)
